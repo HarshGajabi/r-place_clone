efd760d7dd47b8754bce2f4ee895c271c269c611
tlikhit <58948612+tlikhit@users.noreply.github.com>
2023-12-02 14:35:53 -0500

    efficient fullboard fetch and storage

--- place/server.js
+++ place/server.js
@@ -1,14 +1,30 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8081 });
 
-var dim = 1000; // note: this is not the right dimensions!!
-var board = new Array(dim);
-for (var x = 0; x < dim; x++) {
-	board[x] = new Array(dim);
-	for (var y = 0; y < dim; y++) {
-		board[x][y] = { 'color': 0 };
+var dim = 1000;
+var board = new Array(dim * dim / 2);
+board.fill(0x00);
+
+function getByteIndexAndBitPosition(x, y, dim) {
+	const index = Math.floor((y * dim + x) / 2);
+	const isHigh = (x % 2 === 0); // Even x values affect the high bits, odd affect the low bits
+	return { index, isHigh };
+}
+
+function updateBoard(board, x, y, color) {
+	const { index, isHigh } = getByteIndexAndBitPosition(x, y, dim);
+
+	if (index >= 0 && index < board.length) {
+		if (isHigh) {
+			// Clear the high 4 bits then set them to the new color
+			board[index] = (board[index] & 0x0F) | (color << 4);
+		} else {
+			// Clear the low 4 bits then set them to the new color
+			board[index] = (board[index] & 0xF0) | color;
+		}
 	}
 }
+
 
 wss.on('close', function () {
 	console.log('disconnected');
@@ -44,23 +60,15 @@ wss.on('connection', function (ws) {
 	// heartbeat
 	ws.isAlive = true;
 	ws.on('pong', heartbeat);
-
-	// send initial board: this is slow!!!
-	for (x = 0; x < dim; x++) {
-		for (y = 0; y < dim; y++) {
-			var o = { 'x': x, 'y': y, 'color': board[x][y].color, 'type': 'set' };
-			ws.send(JSON.stringify(o));
-		}
-	}
 	// when we get a message from the client
 	ws.on('message', function (message) {
 		console.log(message);
 		var o = JSON.parse(message);
 		if (isValidSet(o)) {
+			updateBoard(board, o.x, o.y, o.color);
 			console.log("Broadcasting...")
 			o.type = "set";
 			wss.broadcast(JSON.stringify(o));
-			board[o.x][o.y] = { 'color': o.color };
 		} else {
 			console.log("Invalid set: " + message);
 		}
@@ -85,7 +93,11 @@ var app = express();
 // https://expressjs.com/en/starter/static-files.html
 app.use('/', express.static('static_files')); // this directory has files to be returned
 
+// Endpoint to get the board state as binary data
+app.get('/board', function (req, res) {
+	res.end(Buffer.from(board));
+});
+
 app.listen(8080, function () {
 	console.log('Example app listening on port 8080!');
 });
-
--- place/static_files/client.js
+++ place/static_files/client.js
@@ -1,5 +1,6 @@
-const CANVAS_SIZE = 1000;
+const BOARD_SIZE = 1000;
 const WEBSOCKET_URL = `ws://${window.location.hostname}:8081`;
+const BOARD_DATA_URL = `/board`;
 var socket;
 const colors = [
     "white",
@@ -19,6 +20,82 @@ const colors = [
     "navy",
     "black"
 ];
+const colorsRGBA = [
+    [255, 255, 255, 255], // white
+    [192, 192, 192, 255], // silver
+    [128, 128, 128, 255], // gray
+    [0, 255, 0, 255],     // lime
+    [255, 255, 0, 255],   // yellow
+    [0, 255, 255, 255],   // aqua
+    [255, 0, 255, 255],   // fuchsia
+    [255, 0, 0, 255],     // red
+    [0, 0, 255, 255],     // blue
+    [0, 128, 0, 255],     // green
+    [0, 128, 128, 255],   // teal
+    [128, 0, 128, 255],   // purple
+    [128, 0, 0, 255],     // maroon
+    [128, 128, 0, 255],   // olive
+    [0, 0, 128, 255],     // navy
+    [0, 0, 0, 255]        // black
+];
+
+async function fetchBoardData(url) {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    return blob.arrayBuffer();
+}
+
+function processBoardData(arrayBuffer) {
+    const byteArray = new Uint8Array(arrayBuffer);
+    let board = Array.from(Array(BOARD_SIZE), () => new Array(BOARD_SIZE));
+
+    let byteIndex = 0;
+    for (let y = 0; y < BOARD_SIZE; y++) {
+        for (let x = 0; x < BOARD_SIZE; x += 2) { // Increment by 2 as each byte has 2 pixels
+            if (byteIndex < byteArray.length) {
+                const byte = byteArray[byteIndex++];
+                const high = byte >> 4; // Extract the high 4 bits
+                const low = byte & 0x0F; // Extract the low 4 bits
+
+                board[y][x] = high; // Assign the high 4 bits to the first pixel
+                if (x + 1 < BOARD_SIZE) {
+                    board[y][x + 1] = low; // Assign the low 4 bits to the second pixel
+                }
+            }
+        }
+    }
+    return board;
+}
+
+function convertBoardToRGBA(board) {
+    const boardSize = board.length;
+    const rgbaArray = new Uint8ClampedArray(boardSize * boardSize * 4); // 4 entries for each pixel (R, G, B, A)
+
+    board.forEach((row, y) => {
+        row.forEach((value, x) => {
+            const [r, g, b, a] = colorsRGBA[value];
+            const index = (y * boardSize + x) * 4;
+            rgbaArray[index] = r;
+            rgbaArray[index + 1] = g;
+            rgbaArray[index + 2] = b;
+            rgbaArray[index + 3] = a;
+        });
+    });
+    console.log(rgbaArray);
+    return rgbaArray;
+}
+
+function drawBoardOnCanvas(board, canvasId) {
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext('2d');
+    const boardSize = board.length;
+    canvas.width = boardSize;
+    canvas.height = boardSize;
+    const rgbaArray = convertBoardToRGBA(board);
+    const imageData = new ImageData(rgbaArray, boardSize, boardSize);
+    console.log(imageData);
+    ctx.putImageData(imageData, 0, 0);
+}
 
 $(function () {
     // Populate color selection
@@ -26,6 +103,16 @@ $(function () {
     $.each(colors, function (index, color) {
         $colorSelect.append($('<option></option>').val(index).html(color));
     });
+
+
+    fetchBoardData(BOARD_DATA_URL)
+        .then(arrayBuffer => processBoardData(arrayBuffer))
+        .then(board => {
+            drawBoardOnCanvas(board, 'canvas');
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 
     socket = new WebSocket(WEBSOCKET_URL);
     socket.onopen = function (event) {
@@ -40,6 +127,7 @@ $(function () {
         if (o.type == "set") {
             var context = document.getElementById('canvas').getContext('2d');
             // assume that o.color stores a 4-bit color index, o.x and o.y give position
+            console.log("Setting pixel at (" + o.x + ", " + o.y + ") to color " + o.color);
             context.fillStyle = colors[o.color];
             context.fillRect(o.x, o.y, 1, 1);
         } else if (o.type == "timeout") {
