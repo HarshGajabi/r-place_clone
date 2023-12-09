b0cccff015086f0f91edd7086348b9c4e80185bc
tlikhit <58948612+tlikhit@users.noreply.github.com>
2023-12-08 19:46:56 -0500

    s3sync script:take bucketname

--- utils/syncStaticS3.sh
+++ utils/syncStaticS3.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Set the AWS region and S3 bucket name
+# Set the AWS region
 AWS_REGION="us-east-2"
-S3_BUCKET="rplace-static"
 
 # Set the local & S3 folder path
 LOCAL_FOLDER="static"
 S3_FOLDER="static"
 
-# Upload the local folder to S3 bucket
+if [ -z "$1" ]; then
+    echo "Please provide the S3 bucket name as an argument."
+    exit 1
+fi
+
+S3_BUCKET="$1"
+
 aws s3 sync "$LOCAL_FOLDER" "s3://$S3_BUCKET/$S3_FOLDER" --region "$AWS_REGION"
