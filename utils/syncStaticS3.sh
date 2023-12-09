d88336742b0ef1851ffb35cbf61d80b6752dc52d
Harshvardhan Gajabi <harsh.gajabi@mail.utoronto.ca>
2023-12-08 20:36:50 -0500

    Reformat sync script

--- utils/syncStaticS3.sh
+++ utils/syncStaticS3.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Please provide the S3 bucket name as an argument."
+    exit 1
+fi
+
 # Set the AWS region
 AWS_REGION="us-east-2"
 
 # Set the local & S3 folder path
 LOCAL_FOLDER="static"
 S3_FOLDER="static"
-
-if [ -z "$1" ]; then
-    echo "Please provide the S3 bucket name as an argument."
-    exit 1
-fi
-
 S3_BUCKET="$1"
 
 aws s3 sync "$LOCAL_FOLDER" "s3://$S3_BUCKET/$S3_FOLDER" --region "$AWS_REGION"
